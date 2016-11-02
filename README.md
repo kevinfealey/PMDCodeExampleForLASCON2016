eeb8ba72880cd0ad7527d969a87f160054c5a12f
Kfealz <kevin.fealey@aspectsecurity.com>
2016-11-02 15:57:59 -0400

    initial push

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PMDCodeExampleForLASCON2016
 Code example to be detected by https://github.com/kevinfealey/PMDRuleForLASCON2016
+
+This code is designed to be checked for a particular issue by https://github.com/kevinfealey/PMDRuleForLASCON2016
+
+This code is not intended to run, and so it has not been tested. 
---
+++ src/main/java/com/aspectsecurity/automation/example/pmd/testcode/GetEqualsPost.java
@@ -0,0 +1,36 @@
+package com.aspectsecurity.automation.example.pmd.testcode;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PostEqualsGet
+ */
+public class GetEqualsPost extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public GetEqualsPost() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
